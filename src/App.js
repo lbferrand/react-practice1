4eb3784a9b273bf9f2604c3ec3fec6a156c10157
lbferrand <lbferrand@gmail.com>
2022-04-15 19:05:17 -0400

    Added content to App js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,42 @@ function App() {
   return (
     <div className="App">
       <h1>Practicing React</h1>
+      <h2>Pro using React</h2>
       <ol>
         <li>Easy to learn</li>
         <li>Modern framework</li>
-        <li>Big popular web develp tool</li>
+        <li>Big popular web developer tool</li>
       </ol>
+
+      <div className="Definition">
+        <h2>Begin with HTML</h2>
+        <p>
+          The first step that I would like to introduce is GitHub Desktop.
+          <ul>
+            <li>Open GitHub Desktop and create a new repository</li>
+            <ol>
+              <li>
+                Visit the following page: https://desktop.github.com/ and
+                install it
+              </li>
+            </ol>
+            <li>Deploy this repository to GitHub (version control)</li>
+            <li>
+              Open the file using VS Code by clicking the option inside GitHub
+              Desktop
+            </li>
+            <li>Open the file 'index.html'</li>
+            <li>
+              Select from the main menu in VS Code - Terminal - New Terminal
+            </li>
+          </ul>
+        </p>
+        <h2>Create a new component using React</h2>
+        <p>
+          A component is a piece of code that will be inside a new file and will
+          be called through a component
+        </p>
+      </div>
     </div>
   );
 }
