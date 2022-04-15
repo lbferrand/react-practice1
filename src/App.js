88d671c4df203e668e3aa21628924b6c0856c0aa
lbferrand <lbferrand@gmail.com>
2022-04-15 15:52:18 -0400

    Testing app.js

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K starts on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
     </div>
   );
 }
