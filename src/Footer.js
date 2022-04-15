0a43c828812b7dfbaa7dd5d02a6c81758fee1cdf
lbferrand <lbferrand@gmail.com>
2022-04-15 18:01:43 -0400

    Created Footer component

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <footer className="App-footer">
+        <span class="copyright-notice">
+          &copy; 2022 Portfolio Luciana B Ferrand. All right reserved.
+        </span>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function Header() {
   return (
-    <div className="App">
+    <div className="Header">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
